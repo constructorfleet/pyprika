f18f84f7f67b0a45c4b00435a77370b085a343db
Teagan glenn <teagan.m.glenn@gmail.com>
2020-01-14 21:52:56 -0700

    Fix type-o

--- pyprika/framework/containers/model_container.py
+++ pyprika/framework/containers/model_container.py
@@ -5,6 +5,6 @@ class ModelContainer:
     """Container for data models."""
     __slots__ = ['bookmarks', 'categories', 'groceries', 'meals', 'menus', 'menu_items', 'pantry', 'recipes', 'status']
 
-    def __init__(self, bookmarks, categories, groceries, meals, menus, menu_items, pantry, recipe, stattus):
+    def __init__(self, bookmarks, categories, groceries, meals, menus, menu_items, pantry, recipe, status):
         """Initialize container."""
         auto_init()
