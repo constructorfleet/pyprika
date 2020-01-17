780f0942b133656eeeabcb05cc43f72e7149048b
Teagan glenn <teagan.m.glenn@gmail.com>
2020-01-16 18:39:28 -0700

    Kill signals

--- pyprika/domain/work_units/backgroud_refresh_data.py
+++ pyprika/domain/work_units/backgroud_refresh_data.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import signal
 import threading
 
 from pyprika.framework.work_unit_base import AsyncWorkUnit
@@ -14,9 +15,13 @@ class BackgroundRefreshData(AsyncWorkUnit):
 
     def __init__(self, fetch_data, interval_seconds):
         """Initialize the unit of work."""
+        signal.signal(signal.SIGABRT, self._signal_handler)
+        signal.signal(signal.SIGKILL, self._signal_handler)
+        signal.signal(signal.SIGINT, self._signal_handler)
+
         self.fetch_data = fetch_data
         self.interval_seconds = interval_seconds
-
+        self._running = True
         self._loop = asyncio.get_event_loop()
         self._thread = threading.Thread(target=self._loop_in_thread)
         self._thread.start()
@@ -28,7 +33,10 @@ class BackgroundRefreshData(AsyncWorkUnit):
         except asyncio.CancelledError:
             pass
 
+    def _signal_handler(self, signum, frame):
+        self._running = False
+
     async def perform_work(self):
-        while True:
+        while self._running:
             await self.fetch_data.perform_work()
             await asyncio.sleep(self.interval_seconds)
